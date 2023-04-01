50f77cb9c9f7fac62b6f59bd6b568d7e33a9c540
Luki <hexeyxd@gmail.com>
2023-04-01 13:28:01 +0200

    typing corrections

--- gpt/text_generator.py
+++ gpt/text_generator.py
@@ -1,8 +1,6 @@
 import openai
 openai.api_key = "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"
 
-category = "frogs"
-n_sentences = 4
 
 def filter_out_sc(input:str)->str:
         special_characters=['@','#','$','*','&',"""\n""",'"','\\',""]
@@ -17,7 +15,7 @@ class PromptGenerator():
         self.response_type_specifier = response_type_specifier
         self.system_rules = "{} should be around 7 words long response should include a subject (person, object, or location) and descriptors (adverbs and adjectives that describe the subject).Avoid using abstract concepts in spite of concrete nouns. Responses whould be in simple english. Avoid adding request context".format(self.response_type_specifier)
     
-   
+
     def generatePrompt(self,category:str,  n_sentences:int, randomness_temperature:float=1.0, accuracy_filter:float=1.0, max_tokens:int=10) -> list[str]:
         response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
