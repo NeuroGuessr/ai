3b24ca8ddb93f51b7652789afa7952e9f08d7ff3
superior_shrimp <szymon.zychowicz300701@gmail.com>
2023-04-02 12:38:23 +0200

    przeniesienie odpowiedzialnosci na category uuid

--- src/CategoryManager.py
+++ src/CategoryManager.py
@@ -2,11 +2,16 @@ from uuid import uuid4
 
 class CategoryManager:
     def __init__(self):
-        self.categories = {} # category name -> category_id
+        self.categories = {} # category_id -> category name
         self.images = {} # category_id -> image list
 
     def get_categories(self) -> dict:
         return self.categories
+
+    def get_category_uuid(self, category_name: str):
+        for key, value in self.categories.items():
+            if value == category_name:
+                return key
 
     def get_images(self, category_uuid: str) -> list:
         return self.images[category_uuid]
@@ -16,6 +21,6 @@ class CategoryManager:
 
     def add_category(self, name: str):
         category_uuid = str(uuid4())
-        self.categories[name] = category_uuid
+        self.categories[category_uuid] = name
         self.images[category_uuid] = []
         
--- src/ImageCollector.py
+++ src/ImageCollector.py
@@ -7,15 +7,15 @@ class ImageCollector:
         self.correct = {} # name -> label
     
     def filter_images(self, key: str) -> dict:
-        category_uuid = self.category_manager.get_categories()[key]
-        return {key + "/" + image: self.correct[key + "/" + image] for image in self.category_manager.get_images(category_uuid)}
+        category_name = self.category_manager.get_categories()[key]
+        return {category_name + "/" + image: self.correct[category_name + "/" + image] for image in self.category_manager.get_images(key)}
     
     def load_data(self):
         with open("../static/index.txt", 'r') as file:
             for triplet in [line.replace('\n', "").split(":") for line in file]:
                 self.correct[triplet[0] + "/" + triplet[1] + ".jpg"] = triplet[2]
-                if triplet[0] not in self.category_manager.get_categories().keys():
+                if triplet[0] not in self.category_manager.get_categories().values():
                     self.category_manager.add_category(triplet[0])
-                category_uuid = self.category_manager.get_categories()[triplet[0]]
+                category_uuid = self.category_manager.get_category_uuid(triplet[0])
                 self.category_manager.tag_image(triplet[1] + ".jpg", category_uuid)
                 
